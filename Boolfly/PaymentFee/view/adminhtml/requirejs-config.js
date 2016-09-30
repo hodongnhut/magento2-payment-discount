430c2dae5bd2dc74dbbc91452acf8f9191f9e46c
Mr Khoa <mrkhoa99@gmail.com>
2016-09-30 18:02:17 +0700

    Add xml files.

---
+++ Boolfly/PaymentFee/view/adminhtml/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    map: {
+        '*': {
+            'Magento_Sales/order/create/scripts':
+                'Boolfly_PaymentFee/js/order/create/scripts'
+        }
+    }
+};
